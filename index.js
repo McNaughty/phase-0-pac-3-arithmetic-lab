45472a979e9dc70381783ca8969738d268222dbf
Winnie <wimacoj@gmail.com>
2023-09-18 20:02:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+function add(a, b){
+    return a + b;
+}
+
+add(1,1347);
+
+function subtract(a, b){
+    return a - b;
+}
+
+subtract(600, 44);
+
+function multiply(a, b){
+    return a * b;
+}
+
+multiply(74120, 6);
+
+function divide(a, b){
+    return a/ b;
+}
+
+add(30.8771, 10);
+
+// NUmber increments
+
+let n = 5;
+
+function increment(n){
+    n++;
+    return n;
+}
+
+
+n = 150;
+
+function decrement(n){
+    --n;
+    return n;
+}
+
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+//preserveDecimal(n)
+       //preserves n's decimals (it parses n as a floating point number) and returns the parsed number:
+
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}       
